9b9400348037fa5f2c263f168b48ce3750287b04
John B <jbonczek1@ivytech.edu>
2022-07-17 16:38:12 -0500

    updates

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
     res.send("Hello!");
+});
+
+app.post('/hello', (req,res) => {
+    res.send(`Hello ${req.body.name}`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.send(`Helo ${req.params.name}`);
 });
 
 app.listen(8000, () => {
